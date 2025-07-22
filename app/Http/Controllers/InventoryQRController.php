18961cd6039b5232fe0639ee5ed7b3eff9c21514
Dimas Galang <dimasgalang@student.com>
2025-07-22 14:48:03 +0700

    fix null

--- app/Http/Controllers/InventoryQRController.php
+++ app/Http/Controllers/InventoryQRController.php
@@ -69,7 +69,7 @@ class InventoryQRController extends Controller
 
     public function batchqr()
     {
-        $inventoryqrs = InventoryQR::where('qr_code', '=', null);
+        $inventoryqrs = InventoryQR::where('qr_code', '=', null)->get();
         foreach ($inventoryqrs as $inventoryqr) {
             $qr_data = $inventoryqr->item_number . "_" . $inventoryqr->assets_number . "_" . $inventoryqr->brand . "_" . $inventoryqr->type . "_" . $inventoryqr->item_name . "_" . $inventoryqr->serial_number . "_" . $inventoryqr->incoming_date . "_" . $inventoryqr->location;
 
